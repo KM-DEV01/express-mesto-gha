89725b2405388c87b26ded48d99cf6ec0dd9365e
kamil_hades <kamil.mardanov04@yandex.ru>
2023-05-08 20:38:32 +0300

    fix: автотест

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64578ea6165102c531203da6', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+  next();
+});
+
 app.use('/', require('./routes/index'));
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,6 @@ const router = require('express').Router();
 
 router.use(express.json());
 
-router.use((req, res, next) => {
-  req.user = {
-    _id: '64578ea6165102c531203da6', // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-  next();
-});
-
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 
