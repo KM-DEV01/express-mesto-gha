9f6b1b68b7c3792d46f88c09d0b338b66fa8326c
kamil_hades <kamil.mardanov04@yandex.ru>
2023-05-09 02:22:47 +0300

    fix: исправление замечаний

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = require('express').Router();
 
+const NOT_FOUND = 404;
+
 router.use(express.json());
 
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
 
-router.use('*', (req, res) => res.status(404).send({ message: '404' }));
+router.use('*', (req, res) => res.status(NOT_FOUND).send({ message: '404' }));
 
 module.exports = router;
