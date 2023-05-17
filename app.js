d871b97f158cf31cb0348eadea13c850d9e5cc89
kamil_hades <kamil.mardanov04@yandex.ru>
2023-05-17 13:10:40 +0300

    feat: добавить модули безопасности

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
 app.use('/', require('./routes/index'));
 
